66a6d5914c378ba39d48352920a06a8a600e431a
Dhriti <dhriti.baruah20@gmail.com>
2024-01-31 18:25:18 +0530

    Changes To payments page

--- booking-app/src/components/Payments.tsx
+++ booking-app/src/components/Payments.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 import logo from "../Images/redbuslogo2.jpg";
 
 const Payments = () => {
   return (
-    <div className='z-20 fixed top-0 left-0 w-[100vw] h-[100vh] bg-white'>
-           <nav className="homeNav flex justify-between">
+    <div className="z-20 fixed top-0 left-0 w-[100vw] h-[100vh] bg-white">
+      <nav className="homeNav flex justify-between">
         <img
           src={logo}
           alt=""
@@ -20,23 +20,74 @@ const Payments = () => {
           </li>
         </ul>
       </nav>
-      <div className='bg-red-500 p-3'>
-        <ul className='text-yellow-200 flex justify-evenly'>
-        <li className='flex'>origin <i className="material-icons mx-3">arrow_forward</i> destination</li>
-        <li>Please pay within timer</li>
+      <div className="bg-red-500 p-3">
+        <ul className="text-yellow-200 flex justify-evenly">
+          <li className="flex">
+            origin <i className="material-icons mx-3">arrow_forward</i>{" "}
+            destination
+          </li>
+          <li className="flex">
+            Please pay within <i className="material-icons mx-2">alarm</i> timer
+          </li>
         </ul>
       </div>
-      <div className='main flex'>
+      <div className="main flex">
         <div className="payment-options w-[60%] p-10"></div>
-        <div className="passenger-info w-[40%] p-10">
-            <div className='shadow-lg shadow-black p-5'>
-            <h1 className='text-red-500'>Bus name</h1>
+        <div className="passenger-info w-[40%] m-10">
+          <div className="shadow-lg shadow-black">
+            <h1 className="text-red-500 font-bold text-xl p-5">Bus name</h1>
             <hr />
+            <div className="departure my-4 flex justify-between">
+              <div className="flex">
+                <div className="p-3">
+                  <i className="material-icons mx-3 text-red-500">event</i>
+                </div>
+                <div>
+                  <h2>Departure</h2>
+                  <h3 className="text-lg font-semibold">doj start_time</h3>
+                </div>
+              </div>
+              <div className="px-5 text-right">
+                <h2>Seats</h2>
+                <h3 className="text-lg font-semibold">seatNo</h3>
+              </div>
             </div>
+            <hr />
+            <div className="drop-off my-4 flex justify-between">
+              <div className="flex">
+                <div className="p-3">
+                  <i className="material-icons mx-3 text-red-500">place</i>
+                </div>
+                <div>
+                  <h2>Boarding Point</h2>
+                  <h3 className="text-lg font-semibold">origin</h3>
+                </div>
+              </div>
+              <div className="px-5 text-right">
+                <h2>Dropping Point</h2>
+                <h3 className="text-lg font-semibold">destination</h3>
+              </div>
+            </div>
+            <hr />
+            <div className="passenger bg-red-500">
+             <p className="flex font-semibold text-md py-5 px-4 text-yellow-300"><i className="material-icons mx-4">account_circle</i>Passenger Name</p> 
+            </div>
+          </div>
+          <div className="shadow-lg shadow-black my-5 p-4 space-y-4">
+            <h1 className="text-red-500 text-xl font-bold px-4">FARE BREAKUP</h1>
+            <ul className="flex justify-between px-4 text-xl">
+                <li>Onward Fare</li>
+                <li>INR 1021</li>
+            </ul>
+            <ul className="flex justify-between px-4 text-xl font-bold">
+                <li>Total Payable</li>
+                <li>INR 1021</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Payments
+export default Payments;
